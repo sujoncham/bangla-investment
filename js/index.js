55f0254750e7d9e4d7dba5ec920c3ac96fef6467
sujoncham <sojonchambugong@gmail.com>
2022-02-19 10:58:41 +0600

    html code added and js function added

--- js/index.js
+++ js/index.js
@@ -2,4 +2,12 @@ function calculate(){
     const incomeInput = document.getElementById('income-input');
     const incomeValue = parseInt(incomeInput.value);
     // console.log(incomeValue);
+    const foodInput = document.getElementById('food-input');
+    const foodValue = parseInt(foodInput.value);
+    const rentInput = document.getElementById('rent-input');
+    const rentValue = parseInt(rentInput.value);
+    const clotheInput = document.getElementById('clothe-input');
+    const clotheValue = parseInt(clotheInput.value);
+    const totalCost = parseInt(foodInput.value)+ parseInt(rentInput.value) +parseInt(clotheInput.value);
+    const totalBalance = parseInt(incomeInput.value) -totalCost;
 }
