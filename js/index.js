e8998a3239137e480281dc4df9ca9c82f3842608
sujoncham <sojonchambugong@gmail.com>
2022-02-19 19:09:02 +0600

    validating condition

--- js/index.js
+++ js/index.js
@@ -1,19 +1,35 @@
 
+
 // this is income and expences part 
 function calculate(){
-    // incomeInput
-    const incomeInput = document.getElementById('income-input');
-    // expence Input
-    const foodInput = document.getElementById('food-input');
-    const rentInput = document.getElementById('rent-input');
-    const clotheInput = document.getElementById('clothe-input');
-    // total cost balance
-    const totalCost = parseInt(foodInput.value) + parseInt(rentInput.value) + parseInt(clotheInput.value);
-
-    const totalBalance = parseInt(incomeInput.value) - totalCost;
-    // showing balance
-    document.getElementById('expence-total').innerText = totalCost;
-    document.getElementById('balance-total').innerText = totalBalance;
+    
+          // incomeInput
+          const incomeInput = document.getElementById('income-input');
+          const incomeValue = parseInt(incomeInput.value);
+          // expence Input
+          const foodInput = document.getElementById('food-input');
+          const foodValue = parseInt(foodInput.value);
+          const rentInput = document.getElementById('rent-input');
+          const rentValue = parseInt(rentInput.value)
+          const clotheInput = document.getElementById('clothe-input');
+          const clotheValue = parseInt(clotheInput.value)
+          let text;
+          if((isNaN(incomeValue) || incomeValue < 0) && (isNaN(foodValue) || foodValue < 0) && (isNaN(rentValue) || rentValue < 0) && (isNaN(clotheValue) || clotheValue > 0)){
+            text = "input invalid";
+          } else{
+            // total cost balance
+          const totalCost = foodValue + rentValue + clotheValue;
+      
+          const totalBalance = incomeValue- totalCost;
+               // showing balance
+          document.getElementById('expence-total').innerText = totalCost;
+          document.getElementById('balance-total').innerText = totalBalance;
+          text = "input valid";
+          
+          }
+          document.getElementById('warning-text').innerText = text;
+          
+    
 }
 
 // this is percentage part 
@@ -27,7 +43,10 @@ function save(){
     // percentage input 
     const saveInputh = document.getElementById('save-input');
     const saveValue = parseInt(saveInputh.value);
-    // percentage innerText value 
+    if(typeof (saveValue.value)!='number'){
+        alert("hello");
+    } else{
+        // percentage innerText value 
     const saveAmount = document.getElementById('save-amount')
     const amountValue = parseInt(saveAmount.innerText);
 
@@ -39,4 +58,6 @@ function save(){
     document.getElementById('save-amount').innerText = totalSave;
     // remaining balance
     document.getElementById('remain-balance').innerText = mainValue - totalSave;
+    }
+    
 }
