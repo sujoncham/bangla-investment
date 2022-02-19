bda0a5f6f082cfc7c73ff210ac95d7f89a18a282
sujoncham <sojonchambugong@gmail.com>
2022-02-19 15:16:08 +0600

    js commented

--- js/index.js
+++ js/index.js
@@ -1,40 +1,42 @@
-function calculate(){
-    const incomeInput = document.getElementById('income-input');
-    const incomeValue = parseInt(incomeInput.value);
-    // console.log(incomeValue);
-    const foodInput = document.getElementById('food-input');
-    // const foodValue = parseInt(foodInput.value);
-    const rentInput = document.getElementById('rent-input');
-    // const rentValue = parseInt(rentInput.value);
-    const clotheInput = document.getElementById('clothe-input');
-    // const clotheValue = parseInt(clotheInput.value);
-    const totalCost = parseInt(foodInput.value) + parseInt(rentInput.value) + parseInt(clotheInput.value);
-    const totalBalance = parseInt(incomeInput.value) - totalCost;
-    // console.log(totalBalance);
 
-   
+// this is income and expences part 
+function calculate(){
+    // incomeInput
+    const incomeInput = document.getElementById('income-input');
+    // expence Input
+    const foodInput = document.getElementById('food-input');
+    const rentInput = document.getElementById('rent-input');
+    const clotheInput = document.getElementById('clothe-input');
+    // total cost balance
+    const totalCost = parseInt(foodInput.value) + parseInt(rentInput.value) + parseInt(clotheInput.value);
+
+    const totalBalance = parseInt(incomeInput.value) - totalCost;
+    // showing balance
     document.getElementById('expence-total').innerText = totalCost;
     document.getElementById('balance-total').innerText = totalBalance;
 }
 
+// this is percentage part 
 function save(){
     // income
     const incomeInput = document.getElementById('income-input');
     const incomeValue = parseInt(incomeInput.value);
-
+    // main balance
     const balance = document.getElementById('balance-total');
     const mainValue = parseInt(balance.innerText);
-
+    // percentage input 
     const saveInputh = document.getElementById('save-input');
     const saveValue = parseInt(saveInputh.value);
-
+    // percentage innerText value 
     const saveAmount = document.getElementById('save-amount')
     const amountValue = parseInt(saveAmount.innerText);
 
-    // console.log(saveValue);
     const save = saveValue + amountValue;
+    // percentage calculate
     const savePercentage = save/100;
     const totalSave = incomeValue*savePercentage;
+    // totalSave
     document.getElementById('save-amount').innerText = totalSave;
+    // remaining balance
     document.getElementById('remain-balance').innerText = mainValue - totalSave;
 }
