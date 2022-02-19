55833f58cb6611088ed423890b73a67904e80361
sujoncham <sojonchambugong@gmail.com>
2022-02-19 10:42:05 +0600

    js file added

--- js/index.js
+++ js/index.js
@@ -1,29 +1,3 @@
-
-function getInputValue(inputValue){
-    let depositInput = document.getElementById(inputValue);
-    let depositInputValue = parseFloat(depositInput.value);
-    depositInput.value = "";
-    return depositInputValue;
-}
-
-function updateInputValue(inputFieldId, inputAmount){
-    let depoTotal = document.getElementById(inputFieldId);
-    let addValue = parseFloat(depoTotal.innerText);
-    depoTotal.innerText = inputAmount + addValue;
-}
-
-function depositValue(){
-    const depositInputValue = getInputValue('deposit-input');
-    updateInputValue('depo-total', depositInputValue)
+function calculate(){
     
-    let depoBalance = document.getElementById('depo-balance');
-    depoBalance.innerText = depositInputValue + parseFloat(depoBalance.innerText);
-}
-
-function widrowValue(){
-    const windrowInputValue = getInputValue('widrow-input');
-    updateInputValue('widrow-total', windrowInputValue)
-    
-    let depoBalance = document.getElementById('depo-balance');
-    depoBalance.innerText = parseFloat(depoBalance.innerText) -  windrowInputValue;
 }
